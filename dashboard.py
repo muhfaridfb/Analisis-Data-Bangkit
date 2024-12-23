5c5f666caa07df1b4a7ed7ef5227346d33217544
muhfaridfb <142194808+muhfaridfb@users.noreply.github.com>
2024-12-24 00:14:38 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,7 @@
+import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import streamlit as st
 
 # Load Data dengan st.cache_data untuk caching
 @st.cache_data
